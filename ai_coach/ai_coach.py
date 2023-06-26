d2199a2eb68112ada939db15884a2c50fde29977
Hina Khadim <hina.khadim@arbisoft.com>
2023-06-26 10:13:39 +0500

    fix: Model selection bug fix, and UI fix which includes adding tooltip, improve Css , add translation

--- ai_coach/ai_coach.py
+++ ai_coach/ai_coach.py
@@ -7,76 +7,98 @@ from django.template import Context, Template
 from web_fragments.fragment import Fragment
 from xblock.completable import CompletableXBlockMixin
 from xblock.core import XBlock
-from xblock.fields import Integer, Scope, String
+from xblock.fields import Float, Integer, Scope, String
 from xblockutils.studio_editable import StudioEditableXBlockMixin
 
 log = logging.getLogger(__name__)
 
+_ = lambda text: text
 
+CHAT_COMPLETION_MODELS = ['gpt-3.5-turbo']
+TEXT_COMPLETION_MODELS = [
+    'text-davinci-003',
+    'text-davinci-002',
+    'text-curie-001',
+    'text-babbage-001',
+    'text-ada-001',
+]
+AI_MODELS = CHAT_COMPLETION_MODELS + TEXT_COMPLETION_MODELS
+
+@XBlock.wants('i18n')
 class AICoachXBlock(XBlock, StudioEditableXBlockMixin, CompletableXBlockMixin):
     """
         AI Coach xblock - Helps student to ask for improvement of their answer once
     """
 
     display_name = String(
-            display_name='Display Name',
-            help='Display name for this module',
+            display_name=_('Display Name'),
+            help=_('Display name for this module'),
             default="AI Coach",
             scope=Scope.settings
     )
 
     question = String(
-            display_name='Question',
+            display_name=_('Question'),
             default='',
             scope=Scope.settings,
             multiline_editor=True,
-            help='The question asked by the teacher',
+            help=_('The question asked by the teacher'),
     )
     student_answer = String(
-            display_name='Answer',
+            display_name=_('Answer'),
             default='',
             scope=Scope.user_state,
-            help='The answer provided by Student'
+            help=_('The answer provided by Student')
     )
 
     context = String(
-            display_name='Context',
+            display_name=_('Context'),
             default="",
             scope=Scope.settings,
             multiline_editor=True,
-            help="Write the question context here",
+            help=_("Write the question context here"),
     )
 
     feedback_threshold = Integer(
-            display_name='Feedback Threshold',
+            display_name=_('Feedback Threshold'),
             default=1, scope=Scope.settings,
-            help="Maximum no. of times student asks for feedback"
+            help=_("Maximum no. of times student asks for feedback")
     )
     feedback_count = Integer(
             default=0, scope=Scope.user_state,
-            help="No. of times student asks for feedback"
+            help=_("No. of times student asks for feedback")
     )
 
     api_key = String(
-            display_name="API Key",
+            display_name=_("API Key"),
             default=settings.OPENAI_SECRET_KEY,
             scope=Scope.settings,
-            help="Your OpenAI API key, which can be found at <a href='https://platform.openai.com/account/api-keys' target='_blank'>https://platform.openai.com/account/api-keys</a>",
+            help=_(
+                "Your OpenAI API key, which can be found at <a href='https://platform.openai.com/account/api-keys' target='_blank'>https://platform.openai.com/account/api-keys</a>"
+                ),
     )
 
     model_name = String(
-            display_name="AI Model Name", values=(
-                'text-davinci-003', 'text-davinci-002', 'text-curie-001', 'text-babbage-001',
-                'text-ada-001'),
+            display_name=_("AI Model Name"), values=AI_MODELS,
             default="text-davinci-003", scope=Scope.settings,
-            help="Select an AI Text model."
+            help=_("Select an AI Text model.")
+    )
+
+    temperature = Float(
+            display_name=_('Temperature'),
+            default=0.5,
+            values={'min': 0.1, 'max': 2, 'step': 0.1},
+            scope=Scope.settings,
+            help=_(
+                'Higher values like 0.8 will make the output more random, while lower values \n like 0.2 will make it more focused and deterministic.'
+                )
     )
 
     description = String(
-            display_name='Description',
+            display_name=_('Description'),
             default='Description here...',
             scope=Scope.settings,
-            help='Any Description'
+            help=_('Any Description')
     )
 
     editable_fields = [
@@ -85,6 +107,7 @@ class AICoachXBlock(XBlock, StudioEditableXBlockMixin, CompletableXBlockMixin):
         'question',
         'model_name',
         'api_key',
+        'temperature',
         'description',
         'feedback_threshold'
     ]
@@ -94,15 +117,20 @@ class AICoachXBlock(XBlock, StudioEditableXBlockMixin, CompletableXBlockMixin):
         data = pkg_resources.resource_string(__name__, path)
         return data.decode("utf8")
 
+    def get_context(self):
+        return {
+            'title': self.display_name,
+            'question': self.question,
+            'student_answer': self.student_answer,
+            'feedback_count': self.feedback_count,
+            'feedback_threshold': self.feedback_threshold,
+        }
+
     def render_template(self, template_path, context):
         """Handy helper for rendering html template."""
         template_str = self.resource_string(template_path)
         template = Template(template_str)
-        context = {
-            'question': self.question,
-            'student_answer': self.student_answer,
-            'can_request_feedback': self.feedback_count < self.feedback_threshold,
-        }
+        context = self.get_context()
         return template.render(Context(context))
 
     def student_view(self, context=None):
@@ -115,13 +143,33 @@ class AICoachXBlock(XBlock, StudioEditableXBlockMixin, CompletableXBlockMixin):
         frag = Fragment(html)
         frag.add_css(self.resource_string("static/css/ai_coach.css"))
         frag.add_javascript(self.resource_string("static/js/src/ai_coach.js"))
-        frag.initialize_js('AICoachXBlock')
+        frag.initialize_js('AICoachXBlock', json_args=self.get_context())
         return frag
+
+    def get_chat_completion(
+            self, prompt='', model='gpt-3.5-turbo', temperature=0.5, max_tokens=150, n=1
+    ):
+        """ Returns the improvement for student answer using ChatGPT Model """
+
+        messages = [{"role": "user", "content": prompt}]
+        try:
+            response = openai.ChatCompletion.create(
+                    messages=messages,
+                    model=model,
+                    temperature=temperature,
+                    max_tokens=max_tokens,
+                    n=n
+            )
+        except Exception as err:
+            log.error(err)
+            return {'error': _('Unable to connect to AI-coach. Please contact your administrator')}
+
+        return {'response': response.choices[0].message['content']}
 
     def get_completion(
             self, prompt='', model='text-davinci-003', temperature=0.5, max_tokens=150, n=1
     ):
-        """ Returns the improvement for student answer """
+        """ Returns the improvement for student answer using Text AI Model """
 
         try:
             response = openai.Completion.create(
@@ -129,23 +177,32 @@ class AICoachXBlock(XBlock, StudioEditableXBlockMixin, CompletableXBlockMixin):
             )
         except Exception as err:
             log.error(err)
-            return {'error': str(err)}
+            return {'error': _('Unable to connect to AI-coach. Please contact your administrator')}
 
-        log.info(response)
         return {'response': response.choices[0].text}
 
     @XBlock.json_handler
     def ask_from_coach(self, data, suffix=''):
 
         if not data['answer']:
-            return {'error': 'Answer must be required'}
+            return {'error': _('Answer must be required')}
+
+        if self.feedback_count >= self.feedback_threshold:
+            return {'error': _("You've exhausted all available chances to ask the coach for help")}
 
         student_answer = data['answer'].strip()
         prompt = self.context.replace('{{question}}', f'"{self.question}"')
         prompt = prompt.replace('{{answer}}', f'"{student_answer}"')
 
         openai.api_key = self.api_key
-        response = self.get_completion(prompt)
+        if self.model_name in CHAT_COMPLETION_MODELS:
+            response = self.get_chat_completion(
+                prompt, self.model_name, self.temperature
+            )
+        elif self.model_name in TEXT_COMPLETION_MODELS:
+            response = self.get_completion(
+                prompt, self.model_name, self.temperature
+            )
 
         if 'error' in response:
             return {'error': response['error']}
@@ -155,14 +212,15 @@ class AICoachXBlock(XBlock, StudioEditableXBlockMixin, CompletableXBlockMixin):
         return {
             'success': True,
             'coach_answer': coach_answer,
-            'can_request_feedback': self.feedback_count < self.feedback_threshold
+            'feedback_count': self.feedback_count,
+            'feedback_threshold': self.feedback_threshold
         }
 
     @XBlock.json_handler
     def submit_answer(self, data, suffix=''):
 
         if not data['answer']:
-            return {'error': 'Answer must be required'}
+            return {'error': _('Answer must be required')}
 
         self.student_answer = data['answer'].strip()
         self.emit_completion(1.0)
