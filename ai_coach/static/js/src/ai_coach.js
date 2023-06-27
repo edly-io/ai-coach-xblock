1a425df014afccda0a9cb8720ccc67eb9246060c
Hina Khadim <112946189+hinakhadim@users.noreply.github.com>
2023-06-27 11:16:13 +0500

    Selected Model usage fix and UI Fixes  (#6)
    refactor: Model selection bug fix, and UI fix which includes adding tooltip, improve Css , add translation

--- ai_coach/static/js/src/ai_coach.js
+++ ai_coach/static/js/src/ai_coach.js
@@ -1,59 +1,92 @@
 /* Javascript for AICoachXBlock. */
-function AICoachXBlock(runtime, element) {
+function AICoachXBlock(runtime, element, args) {
+
+    let feedback_count = args.feedback_count;
+    const feedback_threshold = args.feedback_threshold;
 
     const studentAnswer = $('#student-answer', element);
-    const coachAnswer = $('.coach-answer p', element);
-    const messageElement = $('.messages', element);
+    const coachAnswerText = $('.coach-answer-text', element);
+    const coachAnswerContainer = $('.coach-answer-container', element);
+    const messageContainerElement = $('.message', element);
+    const messageTextElement = $('.message .message__title', element);
+    const tooltipElement = $('tool-tip', element);
+
+    function showSuccessMessage() {
+        messageContainerElement.addClass('bg-success');
+        messageContainerElement.removeClass('message--hide');
+        messageTextElement.text(gettext('Answer submitted successfully'));
+    }
+
+    function showErrorMessage(error) {
+        messageContainerElement.addClass('bg-danger');
+        messageContainerElement.removeClass('message--hide');
+        messageTextElement.text(error);
+    }
+
+    function resetMessages() {
+        messageContainerElement.removeClass('bg-danger bg-success');
+        messageContainerElement.addClass('message--hide');
+        messageTextElement.text('');
+    }
+
+    function updateTooltipText() {
+        let tooltipText = gettext("You've exhausted all available chances to ask the coach for help")
+        if (feedback_count < feedback_threshold) {
+            tooltipText = edx.StringUtils.interpolate(
+                gettext('You have {feedback_chances} chance{pluralize} left for requesting feedback from the coach'),
+                {
+                    feedback_chances: feedback_threshold - feedback_count,
+                    pluralize: feedback_threshold - feedback_count > 1 ? 's' : ''
+                }
+            )
+        }
+        tooltipElement.text(tooltipText);
+    }
+
 
     function showCoachAnswer(result) {
-        removeClassesFromMsgElement();
-
+        resetMessages();
         if (result.hasOwnProperty('error')) {
-            messageElement.addClass('error');
-            messageElement.text(result.error);
+            showErrorMessage(result.error);
             return;
         }
 
-        messageElement.text('');
-        coachAnswer.text(result.coach_answer);
-
-        if (!result.can_request_feedback) {
-            $('.ask-from-coach', element).hide();
+        coachAnswerText.text(result.coach_answer);
+        if (result.feedback_count >= result.feedback_threshold) {
+            $('.btn-ask-from-coach', element).addClass('disabled');
         }
-        $('.coach-answer ', element).show();
+
+        feedback_count = result.feedback_count;
+        coachAnswerContainer.removeClass('d-none');
     }
 
     function submitAnswer(result) {
-        removeClassesFromMsgElement();
-
+        resetMessages();
         if (result.hasOwnProperty('error')) {
-            messageElement.addClass('error');
-            messageElement.text(result.error);
+            showErrorMessage(result.error);
             return;
         }
 
-        messageElement.text('Answer submitted successfully');
-        messageElement.addClass('success');
+        showSuccessMessage();
     }
 
-    function removeClassesFromMsgElement() {
-        messageElement.removeClass('error');
-        messageElement.removeClass('success');
-    }
+    /* ---------------------------MAKING REQUESTS TO XBLOCK BACKEND-------------------- */
 
     var askFromCoachUrl = runtime.handlerUrl(element, 'ask_from_coach');
     var submitAnswerUrl = runtime.handlerUrl(element, 'submit_answer');
 
-    $('.ask-from-coach', element).click(function (eventObject) {
-        $.ajax({
-            type: "POST",
-            url: askFromCoachUrl,
-            data: JSON.stringify({ 'answer': studentAnswer.val() }),
-            success: showCoachAnswer
-        });
+    $('.btn-ask-from-coach', element).click(function (eventObject) {
+        if (!$(this).hasClass('disabled')) {
+            $.ajax({
+                type: "POST",
+                url: askFromCoachUrl,
+                data: JSON.stringify({ 'answer': studentAnswer.val() }),
+                success: showCoachAnswer
+            });
+        }
     });
 
-    $('.submit-answer', element).click(function (eventObject) {
+    $('.btn-submit-answer', element).click(function (eventObject) {
         $.ajax({
             type: "POST",
             url: submitAnswerUrl,
@@ -64,5 +97,7 @@ function AICoachXBlock(runtime, element) {
 
     $(function ($) {
         /* Here's where you'd do things on page load. */
+        /* ---------------------------UPDATE STYLES ON USER EVENTS-------------------- */
+        $('.btn-ask-from-coach', element).hover(updateTooltipText);
     });
 }
